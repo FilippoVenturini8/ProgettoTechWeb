7253acf37c5bbaa83832954a5dff9e8ac2d6ae60
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2021-12-03 15:30:47 +0100

    Modified create table and started inserting data

---
+++ db/createTable.sql
@@ -0,0 +1,68 @@
+create database lpshop;
+use lpshop;
+
+create table ACCOUNT (
+     Mail varchar(30) not null,
+     Psw varchar(256) not null,
+     Nome varchar(15) not null,
+     Cognome varchar(15) not null,
+     Cellulare int not null,
+     ImmagineProfilo varchar(40),
+     constraint ID_ACCOUNT_ID primary key (Mail));
+
+create table ARTISTA (
+     Nome varchar(30) not null,
+     constraint ID_ARTISTA_ID primary key (Nome));
+
+create table CATEGORIA (
+     Nome varchar(15) not null,
+     constraint ID_CATEGORIA_ID primary key (Nome));
+
+create table DISCO (
+     Codice int not null AUTO_INCREMENT,
+     Titolo varchar(30) not null,
+     DataPubblicazione date not null,
+     QuantitaDisponibile int not null,
+     Copertina varchar(40),
+     Prezzo int not null,
+     VotoMedio int,
+     Artista varchar(30) REFERENCES Artista(Nome),
+     Categoria varchar(15) REFERENCES Categoria(Nome),
+     constraint ID_DISCO_ID primary key (Codice));
+
+create table DISCO_ORDINATO (
+     CodiceDisco int REFERENCES Disco(Codice),
+     CodiceOrdine int REFERENCES Ordine(Codice),
+     Quantita int not null,
+     Voto int,
+     constraint ID_DISCO_ORDINATO_ID primary key (CodiceDisco, CodiceOrdine));
+
+create table DISCO_IN_CARRELLO (
+     CodiceDisco int REFERENCES Disco(Codice),
+     Quantita int not null,
+     MailAccount varchar(30) REFERENCES Account(Mail),
+     constraint ID_DISCO_IN_CARRELLO_ID primary key (MailAccount, CodiceDisco));
+
+create table NOTIFICA (
+     Codice int not null AUTO_INCREMENT,
+     Testo varchar(256) not null,
+     Link varchar(30),
+     Visualizzata varchar(1) not null,
+     MailAccount varchar(30) REFERENCES Account(Mail),
+     constraint ID_NOTIFICA_ID primary key (Codice));
+
+create table ORDINE (
+     Codice int not null AUTO_INCREMENT,
+     CodicePagamento int REFERENCES Pagamento(Codice),
+     DataOrdine date not null,
+     DataSpedizione date,
+     DataConsegna date,
+     Stato varchar(1) not null,
+     MailAccount varchar(1) REFERENCES Account(Mail),
+     constraint ID_ORDINE_ID primary key (Codice),
+     constraint SID_ORDIN_PAGAM_ID unique (CodicePagamento));
+
+create table PAGAMENTO (
+     Codice int not null AUTO_INCREMENT,
+     Importo int not null,
+     constraint ID_PAGAMENTO_ID primary key (Codice));
---
+++ db/insert.sql
@@ -0,0 +1,58 @@
+/*CATEGORIE*/
+INSERT INTO Categoria(Nome)
+VALUES ("Indie"),
+        ("Musica Classica"),
+        ("Punk"),
+        ("Raggae"),
+        ("Rap"),
+        ("Rock");
+
+/*ARTISTI*/
+INSERT INTO Artista(Nome)
+VALUES("Pink Floyd"),
+        ("Salmo"),
+        ("Dire Straits"),
+        ("Nirvana"),
+        ("Red Hot Chili Peppers"),
+        ("ACDC"),
+        ("Linkin Park"),
+        ("U2"),
+        ("Beethoven"),
+        ("Ludovico Einaudi"),
+        ("Mozart"),
+        ("Ezio Bosso"),
+        ("Vivaldi"),
+        ("Pinguini Tattici Nucleari"),
+        ("Ernia"),
+        ("Frah Quintale"),
+        ("Ariete"),
+        ("The Offspring"),
+        ("Green Day"),
+        ("Blink182"),
+        ("Sum41"),
+        ("Alborosie"),
+        ("Bob Marley"),
+        ("Drake"),
+        ("Travis Scott");
+
+/*DISCHI*/
+INSERT INTO Disco(Titolo, DataPubblicazione, QuantitaDisponibile, Copertina, Prezzo, VotoMedio, Artista, Categoria)
+VALUES ("In a time lapse", '2013-01-21', 26, null, 17.99, null, 'Ludovico Einaudi', 'Musica Classica'),
+("The 12th Room", '2015-10-30', 26, null, 28.89, null, 'Ezio Bosso', 'Musica Classica'),
+("Ahia", '2020-12-04', 26, null, 21.51, null, 'Pinguini Tattici Nucleari', 'Indie'),
+("Gemelli", '2020-06-19', 26, null, 23.67, null, 'Ernia', 'Indie'),
+("Regardez Moi", '2017-11-24', 26, null, 26.91, null, 'Frah Quintale', 'Indie'),
+("Americana", '1998-11-17', 26, null, 25.23, null, 'The Offsprings', 'Punk'),
+("American Idiot", '2004-09-20', 26, null, 31.29, null, 'Green Day', 'Punk'),
+("California", '2016-07-01', 26, null, 25.99, null, 'Blink182', 'Punk'),
+("Chuck", '2004-10-12', 26, null, 23.99, null, 'Sum41', 'Punk'),
+("Freedom & Fyah", '2016-05-20', 26, null, 20.81, null, 'Alborosie', 'Reggae'),
+("Legend", '1984-05-08', 26, null, 17.90, null, 'Bob Marley', 'Reggae'),
+("Uprising", '1980-06-10', 26, null, 26.46, null, 'Bob Marley', 'Reggae'),
+("Nothing Was The Same", '2013-09-24', 26, null, 29.99, null, 'Drake', 'Rap'),
+("Playlist", '2018-11-09', 26, null, 29.99, null, 'Salmo', 'Rap'),
+("Astroworld", '2018-08-03', 26, null, 25.72, null, 'Travis Scott', 'Rap'),
+("Californication", '1999-06-08', 26, null, 35.22, null, 'Red Hot Chili Peppers', 'Rock'),
+("Higway To Hell", '1979-08-27', 26, null, 23.49, null, 'ACDC', 'Rock'),
+("Minutes To Midnite", '2007-09-14', 26, null, 25.68, null, 'Linkin Park', 'Rock'),
+("The Dark Side Of The Moon", '1973-03-01', 26, null, 41.50, null, 'Pink Floyd', 'Rock');
