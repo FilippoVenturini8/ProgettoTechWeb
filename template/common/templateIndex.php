77b866c8415c6948809282f0e901deab35833adb
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2021-12-06 11:29:44 +0100

    Add carousel

--- template/common/templateIndex.php
+++ template/common/templateIndex.php
@@ -4,9 +4,40 @@
         Popolari
     </h1>
 </div>
+
 <!--carosello-->
-<div class="row">
+
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="../../img/LP/raggae/Legend-BobMarley.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption">
+                <h5>Legend - Bob Marley</h5>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="../../img/LP/rock/HighwayToHell-ACDC.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption">
+                <h5>Highway To Hell - ACDC</h5>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="../../img/LP/rap/NothingWasTheSame-Drake.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption">
+                <h5>Nothing Was The Same - Drake</h5>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+    </button>
 </div>
+
 <div class="row">
     <div class="col-1"></div>
     <h1 class="col-11">
