34b3ce409ab042cce9a0f287eff91844247133fe
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2021-12-08 11:58:18 +0100

    add map image

--- template/user/templatePayment.php
+++ template/user/templatePayment.php
@@ -1,13 +1,25 @@
 <h1>Pagamento</h1>
+<h2>Indirizzo di spedizione:</h2>
+<div class="text-center">
+    <img src="../../img/icon/spedizione.png" class="icon-map" alt="" />
+</div>
+
+<p>Via Cesare Pavese, 50, Cesena, FC</p>
 
 <div class="row">
-    <!--<img src="../../img/icon/credit-card.png" alt=""/>-->
-    <div class="col-6">
-        <label>Carta di credito</label>
+    <div class="col-4">
+        <label>Importo:</label>
+    </div>
+    <div class="col-2">
+        <label>39,90€</label>
     </div>
 </div>
+
 <div class="row">
+    <div class="col-4">
+        <label>Carta di credito:</label>
+    </div>
     <div class="col-6">
-        <input type="text">
+        <label>1234567890123456</label>
     </div>
 </div>
