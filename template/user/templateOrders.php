d1b73f416268a98b319eb10f546b8bf95e6a50e0
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2021-12-22 15:54:09 +0100

    Order fix

--- template/user/templateOrders.php
+++ template/user/templateOrders.php
@@ -8,23 +8,25 @@
     <!--singolo ordine-->
     <?php foreach($templateParams["orders"] as $order) :?>
         <div class="row p-2 mt-2 border-bottom border-danger">
-            <div class="col-4 py-2 px-4">
-                <div id="carousel<?php echo $order["Codice"] ?>" class="carousel slide" data-interval="false" style="width: 180px;">
+            <div class="col-4 py-2 px-4 d-flex flex-wrap align-items-center">
+                <div id="carousel<?php echo $order["Codice"] ?>" class="carousel slide" data-interval="false" style="width: 150px;">
                     <div class="carousel-inner w-100">
                         <?php $i=0; foreach($templateParams["ordersDetails"][$order["Codice"]] as $disk) :?>
                             <div class="carousel-item <?php if($i==0){ echo "active";}?> w-100">
                                 <img src="<?php echo UPLOAD_DIR.$disk["Copertina"]?>" class="diskInOrder" alt="...">
                             </div>
                         <?php $i=$i+1; endforeach; ?>
+                        <?php if($i > 1): ?>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carousel<?php echo $order["Codice"] ?>" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carousel<?php echo $order["Codice"] ?>" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        <?php endif;?>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel<?php echo $order["Codice"] ?>" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carousel<?php echo $order["Codice"] ?>" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
                 </div>
             </div>
             <div class="col-8 mt-2 px-4">
@@ -39,9 +41,13 @@
                 </div>
                 
                 <p class="text-end m-0 mb-3"><?php echo $order["DataOrdine"]?></p>
-                <?php foreach($templateParams["ordersDetails"][$order["Codice"]] as $disk) :?>
-                    <p>| <?php echo $disk["Quantita"]?>x <?php echo $disk["Artista"]?> - <?php echo $disk["Titolo"]?></p>
-                <?php endforeach; ?>
+
+                <ul class="list-group list-group-flush mb-3">
+                    <?php foreach($templateParams["ordersDetails"][$order["Codice"]] as $disk) :?>
+                        <li class="list-group-item"><?php echo $disk["Quantita"]?>x <?php echo $disk["Artista"]?> - <?php echo $disk["Titolo"]?></li>
+                    <?php endforeach; ?>
+                </ul>
+                    
                 <a href="../../php/user/trackMyPackage.php?idOrder=<?php echo $order["Codice"]?>" class="btn btn-primary float-end">Traccia il mio pacco</a>
             </div>
         </div>
