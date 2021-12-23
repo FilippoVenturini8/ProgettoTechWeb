0d12be800ca49bf3630b622d2b835d755f2d7acb
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2021-12-23 15:03:52 +0100

    change database account

--- template/common/base.php
+++ template/common/base.php
@@ -162,7 +162,7 @@
                 <div class="row">
                     <div class="col-3"></div>
                     <div class="col-6 text-center">
-                        <?php if($_SESSION["immagineprofilo"] != NULL):?>
+                        <?php if(isset($_SESSION["immagineprofilo"]) && $_SESSION["immagineprofilo"] != NULL):?>
                             <img src="../../img/icon/<?php echo($_SESSION["immagineprofilo"])?>" alt=""/>
                         <?php else : ?>
                             <img src="../../img/icon/User.png" alt=""/>
