b2b19477b1d0f2f6e128102519cf5f957c8fa2bb
Malucelli Nicolo <nicolo.malucelli@studio.unibo.it>
2021-12-27 17:20:22 +0100

    handle error 401

--- template/common/templateError401.php
+++ template/common/templateError401.php
@@ -1,5 +1,5 @@
 <div class="text-center">
-    <h1>Error 403</h1>
+    <h1>Error 401</h1>
     <p class="mb-0">Siamo spiacenti ma non siamo stati in grado di autenticarti. </p>
     <p>Effettua il <a href="../../php/common/login.php">Login</a> come admin per poter accedere a questa pagina.</p>
 </div>
